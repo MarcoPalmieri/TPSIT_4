314c6d6b72e4bf3086f57db28e7d02d751e14063
MarcoPalmieri <marcopalmieriap@gmail.com>
2017-01-06 20:58:27 +0100

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,12 @@
+
+package PalmieriMarcoMontaniTpsit4INA;
+//import java.io.*;
+
+public class JavaApplication1 {
+
+   
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+    
+}
